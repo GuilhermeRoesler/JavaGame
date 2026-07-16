3126f084483199c6c758e50336a0a5fc8496de22
Guilherme <guiroesler2@gmail.com>
2026-07-16 00:54:53 -0300

    feat: add car image to game

--- Player.java
+++ Player.java
@@ -1,21 +1,36 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
+    BufferedImage image;
 
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = gp.screenWidth / 10 * 8;
         y = gp.screenHeight / 2 - gp.tileSize / 2;
         speed = 4;
+    }
+
+    public void getPlayerImage() {
+        try {
+            this.image = ImageIO.read(new File("./img/carrinho_sprites_1.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
@@ -36,7 +51,7 @@ public class Player extends Entity {
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
         g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
